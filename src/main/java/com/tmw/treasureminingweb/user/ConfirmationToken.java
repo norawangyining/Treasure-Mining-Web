c40e012b5a307ee5a3688e510a9e8214fb4757cd
Chen Yu <39606745+Chenyu0000@users.noreply.github.com>
2021-07-03 21:07:19 +0800

    Impl ConfirmationTokenRepository

---
+++ src/main/java/com/tmw/treasureminingweb/user/ConfirmationToken.java
@@ -0,0 +1,33 @@
+package com.tmw.treasureminingweb.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfirmationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String confirmationToken;
+
+    private LocalDate createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    ConfirmationToken(User user) {
+        this.user = user;
+        this.createdDate = LocalDate.now();
+        this.confirmationToken = UUID.randomUUID().toString();
+    }
+}
---
+++ src/main/java/com/tmw/treasureminingweb/user/ConfirmationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.tmw.treasureminingweb.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+}
